a1eff1b4783611549dc7a5414cbde175c23fefa9
limafabioh <72600057+limafabioh@users.noreply.github.com>
2020-12-11 19:05:39 -0300

    Update __init__.py

--- arnet/__init__.py
+++ arnet/__init__.py
@@ -22,7 +22,7 @@ from fastai.tabular.all import *
 
 from .ar_net import ARNet
 
-# from .ar_net_legacy import init_ar_learner
+from .ar_net_legacy import init_ar_learner
 from .utils_data import load_from_file, tabularize_univariate, estimate_noise, split_by_p_valid
 from .utils import pad_ar_params, nice_print_list, compute_sTPE, coeff_from_model
 from .plotting import plot_weights, plot_prediction_sample, plot_error_scatter
